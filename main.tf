cda42118f98ae451c1c91a9be32b60ad75252419
Manuel Contrera <manuelcontrera@gmail.com>
2021-07-26 21:09:50 -0300

    agregado de tags y private en output

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
 provider "aws" {
   version = "~> 2.7"
-  region  = "us-east-2"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "web" {  
-  ami = "ami-077e31c4939f6a2f3"
+resource "aws_instance" "web" {
+  count = 2
+  ami = "ami-0dc2d3e4c0f9ebd18"
   instance_type = "t2.micro"
+  
+  tags = {
+    Name = "devops-${count.index}"
+  }
 }
 
 output "public_ip" {
-  value = "${aws_instance.web.public_ip}"
+  value = "${aws_instance.web.*.public_ip}"
+}
+
+output "private_ip" {
+  value = "${aws_instance.web.*.private_ip}"
 }
